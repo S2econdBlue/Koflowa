abd059a3f8083c00aab0bab33b4457e91327d21d
mhlee <mihyun0921@gmail.com>
2022-10-17 10:53:06 +0900

    setting - springboot 프로젝트 생성

---
+++ back/src/main/java/com/d202/koflowa/KoflowaApplication.java
@@ -0,0 +1,13 @@
+package com.d202.koflowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoflowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoflowaApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/d202/koflowa/KoflowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d202.koflowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoflowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

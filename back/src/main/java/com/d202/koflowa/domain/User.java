f67dbfb1709bd0d78e148fb5748f49762911d2f8
mhlee <mihyun0921@gmail.com>
2022-10-25 12:52:11 +0900

    [#4] setting : user table에 id 컬럼 추가

--- back/src/main/java/com/d202/koflowa/domain/User.java
+++ back/src/main/java/com/d202/koflowa/domain/User.java
@@ -1,4 +1,17 @@
 package com.d202.koflowa.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

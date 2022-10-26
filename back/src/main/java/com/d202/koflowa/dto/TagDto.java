b72c3a6e35692615a2e1f2cd3997a5b16c868794
mhlee <mihyun0921@gmail.com>
2022-10-26 12:56:41 +0900

    [#9] feature : TagDto 생성

--- back/src/main/java/com/d202/koflowa/dto/TagDto.java
+++ back/src/main/java/com/d202/koflowa/dto/TagDto.java
@@ -1,4 +1,40 @@
 package com.d202.koflowa.dto;
 
+import com.d202.koflowa.domain.Tag;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TagDto {
+    public static class Request {
+        private Long seq;
+        private String name;
+        private String discription;
+        public Tag toEntity() {
+            return Tag.builder()
+                    .seq(seq)
+                    .name(name)
+                    .discription(discription)
+                    .build();
+        }
+    }
+
+    public static class Response {
+        private Long seq;
+        private String name;
+        private String discription;
+        private LocalDateTime createdTime, modifiedTime;
+
+        public Response(Tag tag) {
+            this.seq = tag.getSeq();
+            this.name = tag.getName();
+            this.discription = tag.getDiscription();
+            this.createdTime = tag.getCreatedTime();
+            this.modifiedTime = tag.getModifiedTime();
+        }
+    }
 }

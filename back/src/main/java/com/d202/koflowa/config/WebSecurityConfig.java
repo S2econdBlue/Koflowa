659e4d7b6456cc79a270cc011b994435cdf8fc13
LJH <ljjuni1114@gmail.com>
2022-10-28 11:01:34 +0900

    [#14] fix : jwt 오류 주석 처리

--- back/src/main/java/com/d202/koflowa/config/WebSecurityConfig.java
+++ back/src/main/java/com/d202/koflowa/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.d202.koflowa.config;
-
-public class SecurityConfig extends WebSecurityConfigurer{
-}
+//package com.d202.koflowa.config;
+//
+//public class SecurityConfig extends WebSecurityConfigurer{
+//}

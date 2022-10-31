7812cc1060d4f45c2525958ca12f64554d314fdd
이청 <s2econd.blue@gmail.com>
2022-10-31 18:00:13 +0900

    feature : redis cluster 구성 및 테스트 중간 작업

---
+++ back/src/main/java/com/d202/koflowa/service/RedisTestService.java
@@ -0,0 +1,9 @@
+package com.d202.koflowa.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisTestService {
+
+
+}
---
+++ back/src/test/java/com/d202/koflowa/testRedisConnection.java
@@ -0,0 +1,54 @@
+package com.d202.koflowa;
+
+import com.d202.koflowa.dto.redisTest.RedisTest;
+import com.d202.koflowa.repository.redisTest.RedisTestRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class testRedisConnection {
+    @Autowired
+    private RedisTestRepository redisTestRepository;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    void add(){
+        int a = 10;
+        System.out.println(a);
+        a += 20;
+        System.out.println(a);
+    }
+    @Test
+    void test(){
+        redisTemplate.getConnectionFactory().getConnection().info().toString();
+
+//        for(int i = 0; i < 2; i++){
+//            redisTemplate.opsForValue().set(i,"chung" + i);
+//        }
+//
+//        for(int i = 0; i < 2; i++){
+//            String returnValue = (String) redisTemplate.opsForValue().get(i);
+//            System.out.println(returnValue);
+//        }
+
+//        redisTemplate.opsForValue().set(0,"chung" + 0);
+        String returnValue = (String) redisTemplate.opsForValue().get("a");
+        System.out.println(returnValue);
+
+
+//        RedisTest redisTest = new RedisTest("abcd", "Chung", 22, LocalDateTime.now());
+//
+//        redisTestRepository.save(redisTest);
+//
+//        redisTestRepository.findById(redisTest.getId());
+//
+//        redisTestRepository.count();
+//
+//        redisTestRepository.delete(redisTest);
+    }
+}

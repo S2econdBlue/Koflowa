ca7b4ef988c0a0770a1f7f517a1669301cd43de6
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-10-31 14:20:22 +0900

    settings : 페이지 이동 예시 작성(App.js) - notfound 페이지 미완

--- front/src/components/page/TalkDetailPage/TalkDetailPage.jsx
+++ front/src/components/page/TalkDetailPage/TalkDetailPage.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class TalkDetailPage extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        대충 태그 디테일 이라는 내용
+        <div>이건 해당태그 질문들이라는 내용</div>
+      </div>
+    );
   }
 }
 

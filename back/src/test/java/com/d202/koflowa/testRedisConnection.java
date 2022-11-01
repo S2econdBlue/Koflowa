37e249cba2d260ee0e33cd4bb0e23761db227a68
Chung Lee <s2econd.blue@gmail.com>
2022-11-01 10:40:51 +0900

    Update testRedisConnection.java

--- back/src/test/java/com/d202/koflowa/testRedisConnection.java
+++ back/src/test/java/com/d202/koflowa/testRedisConnection.java
@@ -1,56 +1,56 @@
-package com.d202.koflowa;
+// package com.d202.koflowa;
 
-import com.d202.koflowa.dto.redisTest.RedisTest;
-import com.d202.koflowa.repository.redisTest.RedisTestRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
+// import com.d202.koflowa.dto.redisTest.RedisTest;
+// import com.d202.koflowa.repository.redisTest.RedisTestRepository;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.data.redis.core.RedisTemplate;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+// import java.time.LocalDateTime;
+// import java.util.Optional;
 
-@SpringBootTest
-public class testRedisConnection {
-    @Autowired
-    private RedisTestRepository redisTestRepository;
+// @SpringBootTest
+// public class testRedisConnection {
+//     @Autowired
+//     private RedisTestRepository redisTestRepository;
 
-    @Autowired
-    private RedisTemplate redisTemplate;
-    @Test
-    void add(){
-        int a = 10;
-        System.out.println(a);
-        a += 20;
-        System.out.println(a);
-    }
-    @Test
-    void test(){
-//        redisTemplate.getConnectionFactory().getConnection().info().toString();
-
-
-//        for(int i = 0; i < 2; i++){
-//            redisTemplate.opsForValue().set(i,"chung" + i);
-//        }
-//
-//        for(int i = 0; i < 2; i++){
-//            String returnValue = (String) redisTemplate.opsForValue().get(i);
-//            System.out.println(returnValue);
-//        }
-
-//        redisTemplate.opsForValue().set(0,"chung" + 0);
-//        String returnValue = (String) redisTemplate.opsForValue().get("a");
-//        System.out.println(returnValue);
+//     @Autowired
+//     private RedisTemplate redisTemplate;
+//     @Test
+//     void add(){
+//         int a = 10;
+//         System.out.println(a);
+//         a += 20;
+//         System.out.println(a);
+//     }
+//     @Test
+//     void test(){
+// //        redisTemplate.getConnectionFactory().getConnection().info().toString();
 
 
-        RedisTest redisTest = new RedisTest("abcd", "Chung", 22, LocalDateTime.now());
+// //        for(int i = 0; i < 2; i++){
+// //            redisTemplate.opsForValue().set(i,"chung" + i);
+// //        }
+// //
+// //        for(int i = 0; i < 2; i++){
+// //            String returnValue = (String) redisTemplate.opsForValue().get(i);
+// //            System.out.println(returnValue);
+// //        }
 
-        redisTestRepository.save(redisTest);
+// //        redisTemplate.opsForValue().set(0,"chung" + 0);
+// //        String returnValue = (String) redisTemplate.opsForValue().get("a");
+// //        System.out.println(returnValue);
 
-//        Optional<RedisTest> redisTest2 = redisTestRepository.findById(redisTest.getId());
-//        System.out.println(redisTest2);
 
-//        redisTestRepository.count();
-//        redisTestRepository.delete(redisTest);
-    }
-}
+//         RedisTest redisTest = new RedisTest("abcd", "Chung", 22, LocalDateTime.now());
+
+//         redisTestRepository.save(redisTest);
+
+// //        Optional<RedisTest> redisTest2 = redisTestRepository.findById(redisTest.getId());
+// //        System.out.println(redisTest2);
+
+// //        redisTestRepository.count();
+// //        redisTestRepository.delete(redisTest);
+//     }
+// }

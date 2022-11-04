b597b2cfb652620f7ddc4bd1405f8173e1a2af99
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-11-04 10:23:23 +0900

    settings : add redux

--- front/src/index.js
+++ front/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 
 //react-redux 관련 함수
-import { Provider as ReduxProvider } from "react-redux"
-import { store, persistor } from "./redux/store"
+// import { Provider as ReduxProvider } from "react-redux"
+// import { store, persistor } from "./redux/store"
 
 //redux-persist 관련 함수
-import { PersistGate } from "redux-persist/integration/react"
+// import { PersistGate } from "redux-persist/integration/react"
 
 import App from "./App"
 

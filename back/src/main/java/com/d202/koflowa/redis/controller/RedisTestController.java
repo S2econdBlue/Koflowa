0f4492e8520d3b5ccf31502af11f1e81a318511a
Chung Lee <s2econd.blue@gmail.com>
2022-11-04 13:52:43 +0900

    fix : redis 설정 변경

--- back/src/main/java/com/d202/koflowa/redis/controller/RedisTestController.java
+++ back/src/main/java/com/d202/koflowa/redis/controller/RedisTestController.java
@@ -1,37 +1,37 @@
-//package com.d202.koflowa.redis.controller;
-//
-//import com.d202.koflowa.redis.dto.RedisTest;
-//import com.d202.koflowa.redis.service.RedisTestService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping("/redis")
-//@RequiredArgsConstructor
-//public class RedisTestController {
-//
-//    @Autowired
-//    private RedisTestService redisTestService;
-//
-//    @PostMapping("/save")
-//    public ResponseEntity saveTest(@RequestBody RedisTest redisTest) {
-//
-//        if(redisTestService.saveDataTest(redisTest)){
-//            return new ResponseEntity(HttpStatus.OK);
-//        }
-//        else
-//            return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
-//    }
-//
-//    @GetMapping("/get")
-//    public RedisTest getTest(String id){
-//        Optional<RedisTest> t = redisTestService.getDataTest(id);
-//        RedisTest redisTest = t.get();
-//        return redisTest;
-//    }
-//}
+package com.d202.koflowa.redis.controller;
+
+import com.d202.koflowa.redis.dto.RedisTest;
+import com.d202.koflowa.redis.service.RedisTestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/redis")
+@RequiredArgsConstructor
+public class RedisTestController {
+
+    @Autowired
+    private RedisTestService redisTestService;
+
+    @PostMapping("/save")
+    public ResponseEntity saveTest(@RequestBody RedisTest redisTest) {
+
+        if(redisTestService.saveDataTest(redisTest)){
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @GetMapping("/get")
+    public RedisTest getTest(String id){
+        Optional<RedisTest> t = redisTestService.getDataTest(id);
+        RedisTest redisTest = t.get();
+        return redisTest;
+    }
+}

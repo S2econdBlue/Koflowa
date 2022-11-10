ea2956d0811e18336f8aabf3600b8393dd9c468f
mhlee <mihyun0921@gmail.com>
2022-11-10 11:13:40 +0900

    fix : springboot CORS allow all 설정

--- back/src/main/java/com/d202/koflowa/KoflowaApplication.java
+++ back/src/main/java/com/d202/koflowa/KoflowaApplication.java
@@ -1,16 +1,12 @@
 package com.d202.koflowa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableJpaAuditing // JPA Auditing 활성화
 //@EnableRedisRepositories(basePackages = {"com.d202.koflowa.repository.redisTest"})
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class KoflowaApplication {
 
 	public static void main(String[] args) {

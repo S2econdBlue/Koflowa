15a0c625a1712475604749810eb8e33d6bbf2efc
YeonJeongLee00 <yeonjeong.lee00@gmail.com>
2022-11-11 15:04:47 +0900

    feature : reputation score 저장 로직 추가

--- back/src/main/java/com/d202/koflowa/user/service/ReputationService.java
+++ back/src/main/java/com/d202/koflowa/user/service/ReputationService.java
@@ -4,6 +4,7 @@ import com.d202.koflowa.user.domain.ReputationLog;
 import com.d202.koflowa.user.domain.User;
 import com.d202.koflowa.user.dto.ReputationLogDto;
 import com.d202.koflowa.user.repository.ReputationLogRepository;
+import com.d202.koflowa.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReputationService {
     private final ReputationLogRepository reputationLogRepository;
-
+    private final UserRepository userRepository;
+    final int THRESHOLD = 150;
     public ReputationLogDto.Response saveLog(User user, String msg, int score, Long questionSeq){
         ReputationLog log =  ReputationLog.builder()
                 .score(score).
@@ -21,6 +23,11 @@ public class ReputationService {
                 .build();
 
         ReputationLog save = reputationLogRepository.save(log);
+        int reputationScore = user.getReputationScore();
+        if(reputationScore < THRESHOLD && reputationScore + score >= THRESHOLD){
+            // 채팅 로그
+        }
+        user.putReputationScore(reputationScore + score);
         return new ReputationLogDto.Response(save);
     }
 }

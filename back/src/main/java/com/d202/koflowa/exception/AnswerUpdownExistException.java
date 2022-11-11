7ddc055b5cadf714ea0c48f9a0baf095de1c452e
LJH <ljjuni1114@gmail.com>
2022-11-11 14:51:05 +0900

    [#20] fix : exception 추가

---
+++ back/src/main/java/com/d202/koflowa/exception/AnswerUpdownExistException.java
@@ -0,0 +1,7 @@
+package com.d202.koflowa.exception;
+
+public class AnswerUpdownExistException extends RuntimeException{
+    public AnswerUpdownExistException(String message) {
+        super(message);
+    }
+}

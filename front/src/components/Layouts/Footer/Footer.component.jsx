cf623dc516451f54b8a9c79ab82f06fe80810f74
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-11-20 23:08:13 +0900

    styles : 푸터 디자인 변경

--- front/src/components/Layouts/Footer/Footer.component.jsx
+++ front/src/components/Layouts/Footer/Footer.component.jsx
@@ -15,37 +15,37 @@ const Footer = () => {
             <a href={process.env.REACT_APP_PUBLIC_URL} target='_self' rel='noreferrer'>
               <Koflowa />
             </a>
-          </div>
-        </div>
-        <div className='item'>
-          <span className='footer-item'>{"ⓒ 코플로와"}</span>
-          <div className='footer-link-list'>
-            <span>
-              <a
-                href='https://dull-jumbo-928.notion.site/Koflowa-52b44b7f45914ef887303bbda5356f20'
-                className='footerItem'
-              >
-                도움말
-              </a>
-            </span>
-            {" | "}
-            <span>
-              <a href='' className='footerItem'>
-                이용약관
-              </a>
-            </span>
-            {" | "}
-            <span>
-              <a href='' className='footerItem'>
-                개인정보 처리지침
-              </a>
-            </span>
-            {" | "}
-            <span>
-              <a href='' className='footerItem'>
-                청소년 보호 정책
-              </a>
-            </span>
+            <div className='item'>
+              <span className='footer-item'>{"ⓒ 코플로와"}</span>
+              <div className='footer-link-list'>
+                <span>
+                  <a
+                    href='https://dull-jumbo-928.notion.site/Koflowa-52b44b7f45914ef887303bbda5356f20'
+                    className='footerItem'
+                  >
+                    도움말
+                  </a>
+                </span>
+                {" | "}
+                <span>
+                  <a href='' className='footerItem'>
+                    이용약관
+                  </a>
+                </span>
+                {" | "}
+                <span>
+                  <a href='' className='footerItem'>
+                    개인정보 처리지침
+                  </a>
+                </span>
+                {" | "}
+                <span>
+                  <a href='' className='footerItem'>
+                    청소년 보호 정책
+                  </a>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
